9ac64a0209d23c095ef39abc54b5aa23e924a742
Daniela Puiu <dpuiu@dpuiu@jhu.edu>
2026-04-16 13:03:38 -0400

    multiple updates

--- env.sh
+++ env.sh
@@ -6,8 +6,9 @@ export V2=2.7
 export MYSQL_ROOT_PASSWORD=your_secure_password
 export MYSQL_PORT=3306
 export CHESSDB_NAME=CHESS_DB
-export CHESSDB_DB_URL="ftp://ftp.ccb.jhu.edu/pub/dpuiu/CHESS_WEB/CHESSApp_prod_backups.db.sql"
+export CHESSDB_BACKUP_URL="ftp://ftp.ccb.jhu.edu/pub/dpuiu/CHESS_WEB/CHESSApp_prod_backups.db.sql"
 export CHESSAPP_DATA_URL="ftp://ftp.ccb.jhu.edu/pub/dpuiu/CHESS_WEB/CHESSApp_prod_backups.data.tgz"
+export CHESSAPP_DB_URL="ftp://ftp.ccb.jhu.edu/pub/dpuiu/CHESS_WEB/CHESSApp_prod_backups.db.gz"
 
 # Admin
 export FLASK_ADMIN_PORT=5001
@@ -16,7 +17,7 @@ export CHESSDB_ADMIN_PASS=your_admin_password
 export CHESSDB_ADMIN_USER=chess_admin
 
 # Public; FLASK_PUBLIC_PORT only port that can;t change???
-export FLASK_PUBLIC_PORT=5000		
+export FLASK_PUBLIC_PORT=5000
 export FRONTEND_PUBLIC_PORT=5113
 export CHESSDB_PUBLIC_PASS=your_public_password
 export CHESSDB_PUBLIC_USER=chess_public
@@ -25,5 +26,4 @@ export CHESSDB_PUBLIC_USER=chess_public
 export FLASK_DEBUG=0
 export GUNICORN_WORKERS=2
 export VITE_API_BASE_URL="https://dev.sites.idies.jhu.edu/api"
-#export BASE_URL="/chess_app/"
-
+#export VITE_BASE_PATH="https://dev.sites.idies.jhu.edu"
